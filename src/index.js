3d81a2ebdb60479518e945150d98aba689de92ba
datdt198213 <dangdat222@gmail.com>
2022-06-25 16:40:21 +0700

    UPDATE course

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+// connect to DB
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,22 +20,26 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-            app.engine(
-                'hbs',
-                handlebars.engine({
-                    extname: '.hbs',
-                }),
-            );
-            app.set('view engine', 'hbs');
-            app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-            // init Route
-            route(app);
+// init Route
+route(app);
 
-            app.listen(port, () => {
-                console.log(`Example app listening on port ${port}`);
-            });
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
